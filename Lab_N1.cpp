6eae7e2163d09b3f60ad97cb82118b8215578bed
eanor <texan.zakull@gmail.com>
2026-09-23 22:39:17 +0700

    blank_file

---
+++ Lab_N1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip> // setw(NUMBER)
+#include <cmath> // math library
+#include <cstdlib> // rand numer (n = 1 + rand % 6)
+#include <sstream> // ostringstream // stoi & stof
+#include <fstream> // work with files //
+using namespace std;
+void main () {
+}
+
